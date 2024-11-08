ebead57e7df23d95ac2e84758e79969058546091
0xoucanxxx <0xoucanxxx@gmail.comn>
2024-11-08 18:17:15 +0700

    Initial commit: Deployed Hello World canister project for ICP

--- src/hello_world_frontend/src/main.js
+++ src/hello_world_frontend/src/main.js
@@ -1,4 +1,11 @@
 import App from './App';
 import './index.scss';
+import { hello_world_backend } from "../../declarations/hello_world_backend";
 
 const app = new App();
+
+// Agregar event listener para mostrar nombres
+document.getElementById("showNames").addEventListener("click", async () => {
+    const names = await hello_world_backend.submittedNames();
+    document.getElementById("nameList").innerText = names.join(", ");
+});
